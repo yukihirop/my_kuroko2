41608d90ee638dbaabff67148bd70d8ec78148a1
yukihirop <te108186@gmail.com>
2019-12-07 15:35:23 +0900

    Configure capistrano and unicorn

--- Capfile
+++ Capfile
@@ -1,8 +1,10 @@
 # Load DSL and set up stages
 require "capistrano/setup"
-
-# Include default deployment tasks
 require "capistrano/deploy"
+require "capistrano/rbenv"
+require "capistrano/bundler"
+require "capistrano/rails/assets"
+require "capistrano/rails/migrations"
 
 # Load the SCM plugin appropriate to your project:
 #
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,45 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "my_kuroko2"
+set :repo_url, "git@github.com:yukihirop/kuroko2.git"
+set :branch, ENV.fetch('DEPLOY_BRANCH')
+set :deploy_to, '/var/www/kuroko2'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :keep_releases, 5
+set :rbenv_ruby, '2.5.3'
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :bundle_jobs, 4
+set :log_level, :debug
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :default_env, {
+  SCOPE: 'kuroko2'
+}
 
-# Default value for :pty is false
-# set :pty, true
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+  after :publishing, :restart
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+end
